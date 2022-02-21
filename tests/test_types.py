c74c6cda529b97a66f439dae127d3c1c941392ab
Moein Kareshk <mkareshk@outlook.com>
2022-02-21 08:43:50 -0800

    update the tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import List
 
 import pandas as pd
@@ -24,6 +25,14 @@ def test_type_date_end_str():
     assert isinstance(returns_data.date_end_str, str)
 
 
+def test_type_asset_date_start():
+    assert isinstance(returns_data.date_start, datetime.datetime)
+
+
+def test_type_date_end():
+    assert isinstance(returns_data.date_end, datetime.datetime)
+
+
 def test_type_business_day_num():
     assert isinstance(returns_data.business_day_num, int)
 
@@ -44,5 +53,17 @@ def test_type_cum_returns():
     assert isinstance(returns_data.cum_returns, pd.DataFrame)
 
 
+def test_type_total_returns():
+    assert isinstance(returns_data.total_returns, pd.Series)
+
+
 def test_type_mean_sd():
     assert isinstance(returns_data.mean_sd, pd.DataFrame)
+
+
+def test_type_annualized_mean_sd():
+    assert isinstance(returns_data.annualized_mean_sd, pd.DataFrame)
+
+
+def test_type_days_per_year():
+    assert isinstance(returns_data.days_per_year, int)
